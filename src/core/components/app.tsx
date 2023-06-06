59f07c1b5d95e5a12c6b128ed7cffba94665444d
labrac <raquelortegalabrac@hotmail.com>
2023-06-06 19:29:47 +0200

    Add phone.slice.ts

--- src/core/components/app.tsx
+++ src/core/components/app.tsx
@@ -1,13 +1,14 @@
+import { Info } from "../../features/phone/components/Info";
+import { Keyboard } from "../../features/phone/components/Keyboard";
+import { Actions } from "../../features/phone/components/Actions";
 export function App() {
   return (
     <>
-    <Info></Info>
-    <main className="phone">
-      <Keyboard>
-      </Keyboard>
-      <Actions>
-      </Actions>
-    </main>
+      <Info></Info>
+      <main className="phone">
+        <Keyboard></Keyboard>
+        <Actions></Actions>
+      </main>
     </>
-    )
+  );
 }
