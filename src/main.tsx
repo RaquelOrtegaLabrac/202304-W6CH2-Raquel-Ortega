a9e71f7e316b9071404a644abfd08055eb724701
labrac <raquelortegalabrac@hotmail.com>
2023-06-06 18:54:41 +0200

    Add store.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { App } from "./core/components/app";
 import "./styles.css";
 
 ReactDOM.createRoot(document.querySelector(".container") as HTMLElement).render(
